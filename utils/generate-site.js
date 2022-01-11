a0da29a452cee2fd8e2a10403cffc13309aa3c19
mcnairjm19 <mcnairjm1993@gmail.com>
2022-01-11 08:59:06 -0500

    added style sheet

---
+++ utils/generate-site.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File Created'
+            });
+        });
+    });
+};
+
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./dist/index.html', err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                mesage: 'File copied'
+            });
+        });
+    });
+};
+
+module.exports = { writeFile, copyFile };
