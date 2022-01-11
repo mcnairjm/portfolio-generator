67ea02e06b48b20de12922bbe07f26e0433eed47
mcnairjm19 <mcnairjm1993@gmail.com>
2022-01-11 13:27:34 -0500

    clean up

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -17,14 +17,14 @@ const writeFile = fileContent => {
 
 const copyFile = () => {
     return new Promise((resolve, reject) => {
-        fs.copyFile('./dist/index.html', err => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
             if (err) {
                 reject(err);
                 return;
             }
             resolve({
                 ok: true,
-                mesage: 'File copied'
+                mesage: 'Stylesheet created'
             });
         });
     });
